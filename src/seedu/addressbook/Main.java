8373dc93859a80f8642423c5878f4e1f338dfb98
David <daviddalmaso@gmail.com>
2018-02-07 23:50:10 +0800

    Handle exception when storage file is changed to read-only during program execution.

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -84,9 +84,16 @@ public class Main {
         do {
             String userCommandText = ui.getUserCommand();
             command = new Parser().parseCommand(userCommandText);
-            CommandResult result = executeCommand(command);
-            recordResult(result);
-            ui.showResultToUser(result);
+
+            try {
+                CommandResult result = executeCommand(command);
+                recordResult(result);
+                ui.showResultToUser(result);
+            } catch (StorageOperationException e ) {
+                ui.showToUser(e.getMessage());
+            } catch (RuntimeException e) {
+                ui.showToUser(e.getMessage());
+            }
 
         } while (!ExitCommand.isExit(command));
     }
@@ -105,12 +112,14 @@ public class Main {
      * @param command user command
      * @return result of the command
      */
-    private CommandResult executeCommand(Command command)  {
+    private CommandResult executeCommand(Command command) throws StorageOperationException, RuntimeException  {
         try {
             command.setData(addressBook, lastShownList);
             CommandResult result = command.execute();
             storage.save(addressBook);
             return result;
+        } catch (StorageOperationException e) {
+            throw e;
         } catch (Exception e) {
             ui.showToUser(e.getMessage());
             throw new RuntimeException(e);
