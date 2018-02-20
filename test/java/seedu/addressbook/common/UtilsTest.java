eac818c90d43e9f1a088e584969f16abc3296e45
David <daviddalmaso@gmail.com>
2018-02-20 20:06:29 +0800

    Add unit test for the Utils class isAnyNull method

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -3,9 +3,11 @@ package seedu.addressbook.common;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
+import com.sun.org.apache.xpath.internal.operations.String;
 import org.junit.Test;
 
 public class UtilsTest {
@@ -36,6 +38,33 @@ public class UtilsTest {
         assertNotUnique(null, "a", "b", null);
     }
 
+    @Test
+    public void elementsAreNotNull() {
+        //empty list
+        assertNoneNull();
+
+        //single null object
+        assertSomeNull((Object) null);
+
+        //single not null object
+        assertNoneNull("abc");
+        assertNoneNull(new String());
+        assertNoneNull(123);
+
+        //multiple null objects
+        assertSomeNull(null, null);
+        assertSomeNull(null, null, null, null);
+
+        //multiple non null objects
+        assertNoneNull("123", 123, "abc");
+        assertNoneNull(new Integer(1), new String());
+
+        //some null objects and some non null objects
+        assertSomeNull(null, null, 123);
+        assertSomeNull(123, null, 123);
+        assertSomeNull(123, "abc", new Integer(3), null);
+    }
+
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
@@ -43,4 +72,8 @@ public class UtilsTest {
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
+
+    private void assertSomeNull(Object... objects) { assertTrue(Utils.isAnyNull(objects)); }
+
+    private void assertNoneNull(Object... objects) { assertFalse(Utils.isAnyNull(objects)); }
 }
